2b1b65e172dc90f8eb10ff4e4f0a2a511a8efa46
Xyber Nova <xyber-nova@outlook.com>
2025-06-01 21:27:14 +0800

    feat: load config when startup

--- termichan/src/main.rs
+++ termichan/src/main.rs
@@ -1,3 +1,13 @@
+use termichan_config::{load_or_create_config, Config};
+use std::sync::OnceLock;
+
+pub static CONFIG: OnceLock<Config> = OnceLock::new();
+
 fn main() {
-   
+    env_logger::init();
+
+    let config = load_or_create_config(None).expect("Failed to load config.");
+    CONFIG.set(config).expect("CONFIG has already initialized.");
+
+    println!("{:#?}", CONFIG);
 }
